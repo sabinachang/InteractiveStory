787e028b1998961b87e7d9bf0ddd9af6b246a08d
sabinachang <sabchang1@gmail.com>
2018-07-11 13:25:52 +0800

    Send user name from MainActivity to StoryActivity.

--- app/src/main/java/com/example/sabina/interactivestory/MainActivity.java
+++ app/src/main/java/com/example/sabina/interactivestory/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.sabina.interactivestory;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText nameField;
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameField = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameField.getText().toString();
+                startStory(name);
+                
+
+
+            }
+        });
     }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this,StoryActivity.class);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key ,name);
+        startActivity(intent);
+
+    }
+
 }
